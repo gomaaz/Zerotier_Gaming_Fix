8846c3a5d9475ace69798ce238bb92cd920e56e4
gomaaz <daniel@lortie.de>
2025-02-01 18:04:14 +0100

    Update install_zerotier_gaming_fix.bat

--- install_zerotier_gaming_fix.bat
+++ install_zerotier_gaming_fix.bat
@@ -84,7 +84,6 @@ echo ==============================================================
 echo.
 echo.
 schtasks /run /tn "ZeroTier Auto Fix"
-
 echo.
 echo.
 echo ==============================================================
@@ -94,6 +93,56 @@ echo.
 echo You can check if network settings are met in 
 echo C:/zerotier_fix/resources/Check_Network_interfaces.bat
 echo right click -> execute with admin rights.
+echo.
+echo.
+echo.
+echo ==============================================================
+echo [Optional] Optional Part - Changing MTU Size of Controller
+echo ==============================================================
+echo.
+echo If you are the network admin of the ZeroTier controller (my.zerotier.com),
+echo you can change the MTU size to a self defined value,f.e. 1400. A lower MTU might be
+echo better for gaming compared to the default ZeroTier MTU of 2800.
+echo.
+echo Please note: These changes will be applied on-the-fly to all adapters
+echo within the ZeroTier network. This MTU setting is a maximum allowed
+echo packet size before fragmentation occurs. Many games will adapt their
+echo packet size based on the MTU of the operating system or network adapter,
+echo although some games choose their own packet sizes independently.
+echo.
+echo Decide carefully before making any changes, as it may affect
+echo network performance and connectivity.
+echo.
+echo.
+echo.
+echo Would you like to change MTU Size now? You need to be network admin (y/n)
+set /p wantmtu=Your choice:
+
+:: Check if user typed "y" or "yes"
+if /i "%wantmtu%"=="y"   goto CHANGEMTU
+if /i "%wantmtu%"=="yes" goto CHANGEMTU
+
+
+:: If not "y" or "yes", go directly to totheend
+echo You answered no. The critical part will be skipped.
+goto TOTHEEND
+
+:CHANGEMTU
+echo.
+echo.
+echo ==============================================================
+echo Starting MTU Change Program
+echo ==============================================================
+echo.
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "%TARGET_DIR%\update_zerotier_mtu.ps1"
+
+echo.
+echo.
+echo.
+echo.
+:TOTHEEND
+echo.
 echo.
 echo.
 echo dP                                                                           oo                   
